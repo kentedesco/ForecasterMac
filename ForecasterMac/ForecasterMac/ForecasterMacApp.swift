051df8bb56a36ed4f0e52a00d1993c5f459c05ad
kentedesco <41804108+kentedesco@users.noreply.github.com>
2023-08-24 19:14:46 -0400

    Add files via upload

---
+++ ForecasterMac/ForecasterMac/ForecasterMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForecasterMacApp.swift
+//  ForecasterMac
+//
+//  Created by Kenneth Tedesco on 8/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ForecasterMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
